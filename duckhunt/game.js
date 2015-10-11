8cc1a6748b025ad85e83b83e1884d2ea2cfbbc33
keyuewang <keyue.wang@tufts.edu>
2015-10-11 14:45:12 -0400

    now we can see two birds in the canvas

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,21 @@
-// Replace this with your code...
+function init(){
+	var ctx = document.getElementById('game_canvas').getContext('2d');
+    var background_pic = new Image();//Create background picture
+    var bird_1 = new Image();//Create bird_1 img
+    var bird_2 = new Image();//Create bird_2 img
+    
+    background_pic.onload = function(){
+        ctx.drawImage(background_pic,0,0,256,240,0,0,800,600);//Set the background picture
+    };
+    background_pic.src = 'duckhunt-background.gif';
+
+    bird_1.onload = function() {
+        ctx.drawImage(bird_1,0,115,40,40,250,270,80,80);//Set the first bird
+    };
+    bird_1.src = 'duckhunt_various_sheet.png';
+    
+    bird_2.onload = function(){
+        ctx.drawImage(bird_2,128,115,40,40,500,120,80,80); //Set the second bird
+    };
+    bird_2.src = 'duckhunt_various_sheet.png';
+}
